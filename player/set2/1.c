846185e141c464b064c05b878f990ccd2b3b3a7b
kavinkumarms <46814027+kavinkumarms@users.noreply.github.com>
2019-03-25 09:41:37 +0530

    Create 1.c

---
+++ player/set2/1.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(void) {
+	
+	char s[15];
+	scanf("%s",s);
+	if((s[0]=='s')||(s[0]=='S'))
+  
+	{
+		printf("yes");
+    
+	}
+	else
+  
+	{
+		printf("no");
+    
+	}
+  
+	
+		
+	return 0;
+  
+}
